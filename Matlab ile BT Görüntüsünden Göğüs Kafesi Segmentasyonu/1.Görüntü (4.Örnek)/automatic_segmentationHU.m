e3a1516917b9f1acdd56e2b181f261b3b106fe87
SenaNurACAR <103754560+SenaNurACAR@users.noreply.github.com>
2022-04-15 17:34:12 +0300

    Add files via upload

---
+++ Matlab ile BT Görüntüsünden Göğüs Kafesi Segmentasyonu/1.Görüntü (4.Örnek)/automatic_segmentationHU.m
@@ -0,0 +1,38 @@
+%% SEGMENTASYON
+%Dosyadan görüntülerin okunmasý
+clear;
+clc;
+
+for i=613:716
+    
+    dosya=strcat(  '1.3.12.2.1107.5.1.4.95454.30000015090806153912500047',num2str(i),'.dcm');
+    img=dicomread(dosya);
+     %Görüntünün Altýnda istenmeyen kýsmýn Silinmesi
+    treshold=graythresh(img);
+    I=im2bw(img,treshold);
+    Mask=bwareaopen(I, 10000);
+    % Dicom görüntü bilgileri alma
+    info = dicominfo(dosya); 
+    % HU deðerleri hesaplayarak yeni görüntüyü oluþturma.
+    img = double(img)*info.RescaleSlope + info.RescaleIntercept; 
+    WC =150; %Alt limit deðeri
+    WW =200;% Üst Limit deðeri
+     %Verileri yeniden ölçeklendirmesi
+    imgScaled = (double(img)-(WC-0.5))/((WW-0.5));
+    %Görüntü yoðunluðu deðerini ve renk haritasýný ayarlar.
+    imd = imadjust(imgScaled);
+    % kemik deðerleri 1 olduðu için 0.99 üsstünü almasýný saðlýyoruz.
+    thresh=0.99; 
+    Rib=im2bw(imd,thresh); 
+    %Görüntünün Geniþletilmesi
+    SE = strel('square',2);
+    BWsdil = imdilate(Rib,SE);
+    %Görüntüdeki Boþluklarýn Doldurulmasý
+    BWdfill = imfill(BWsdil,'holes');
+    %Görüntüdeki beyaz noktalarý silme
+    BW=bwareaopen(BWdfill, 100);
+    New_img=Mask.*BW;
+    figure(1),imshow(New_img)
+end
+
+
---
+++ Matlab ile BT Görüntüsünden Göğüs Kafesi Segmentasyonu/2.Görüntü (1.Örnek)/Rib_SegmentationOrnek1.m
@@ -0,0 +1,77 @@
+%% SEGMENTASYON
+%Dosyadan görüntülerin okunmasý
+clear;
+clc;
+n=0;
+image1=zeros(512,512,124);
+Gray_img=zeros(512,512,124);
+Ic_maske=ones(512,512,124);
+%Görüntüden dosyalarýn okutulmasý
+for i=421:521
+    n=n+1;
+    dosya=strcat(  '1.3.12.2.1107.5.1.4.95454.30000016040505322227500040',num2str(i),'.dcm');
+    image1(:,:,n)=dicomread(dosya);
+end
+%Yarýçap Belirlenmesi
+ r=130;
+for i=1:103
+    Gray_img=mat2gray(image1(:,:,i));
+    treshold=graythresh(Gray_img);
+    img=im2bw(Gray_img,treshold);
+    %Görüntüden istenmeyen Kýsmýn atýlmasý
+    Alt_silme=bwareaopen(img, 10000);
+    %Gray Görüntü  ile alt kýsmý silinen kýsýmla gray görüntünün çarpýlmasý
+    New_gray=Gray_img.*Alt_silme;
+    %iç maske oluþturulmasý
+    img_fill=imfill(img,4,'holes');
+    L=img_fill-img; % maske elde etmek için akciðerlerin çýkarýlmasý
+    L_fill=imfill(L,8,'holes');
+    LungMask=bwareaopen(L_fill, 2000);
+    se = strel ( 'disk',75);
+    afterOpening = imclose (LungMask, se);
+    maske=imcomplement(afterOpening);%Kullanýlacak maske
+    Ic_maske(:,:,i)=maske; %maskeyi iç maske diye bir üç boyutlu görüntüde tutulmasý
+    %Histogram Eþitleme Yapýlmasý
+    hstq = histeq(New_gray);
+    %Treshold Uygulanmasý
+    thresh=0.98;
+    Rib=im2bw(hstq,thresh);
+    %iç maskede oluþan sorunu Çözme
+    if i<60
+        new_image=Rib.*maske;
+    else if (60<=i)&& (i<98)
+            maske=Ic_maske(:,:,25);
+            new_image=Rib.*maske;
+        else
+            maske=Ic_maske(:,:,20);
+            new_image=Rib.*maske;
+        end
+    end
+    %Gürültüler için medyan filtre uygulanmasý
+    Median=medfilt2(new_image, [3 3]);
+    %ilk 10 görüntü için oluþturulacak yuvarlakk maske
+    [M N]=size(Gray_img);
+    %Görüntünün orta noktalarýnýn seçilmesi
+    midpoint_x=M/2;
+    midpoint_y=N/2;
+    %Yuvarlak bir maske oluþturulmasý
+    [x y]=meshgrid(-(midpoint_x-1):midpoint_x, -(midpoint_y-1):midpoint_y);
+    z=sqrt(x.^2+ y.^2);
+    daire=(z<r);
+    % Ýlk 10 görüntüye uygulatma için koþul
+    if i< 11
+    Im=Median.*daire;
+    else 
+        Im=Median;
+    end    
+    %Görüntüyü onarmak için morfolojik iþlemler uygulanmasý
+     SE = strel('square',2);
+    BWsdil = imdilate(Im,SE); % görüntünün geniþletilmesi
+    BWdfill = imfill(BWsdil,'holes'); % görüntüdeki boþluklarýn doldurulmasý
+    BWnobord = imclearborder(BWdfill,4); % Küçük beyaz noktalarýn silinmesi
+    
+    Im=im2double(BWnobord);
+  figure(1),subplot(1,2,1), imshow(Gray_img),title('Orijinal Görüntü'); 
+  subplot(1,2,2), imshow(Im),title('Kemik Segmentasyonu');   
+end
+
